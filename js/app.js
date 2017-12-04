0e16c721d8617d7fedea519ff8f775367683090b
Julia Wopata <julia.wopata@gmail.com>
2017-12-04 12:43:28 -0600

    add modal to submit a tag value

---
+++ js/app.js
@@ -0,0 +1,27 @@
+function submitNumber() {
+  var testing = 0;
+  $("#display").append('<h1>' + testing + '</h1>');
+  $("#setTag").css('display', 'initial');
+  
+}
+
+// When the user clicks the button, open the modal 
+function addTag() {
+    var modal = document.getElementById('tagModal');
+    console.log("test");
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+  var modal = document.getElementById('tagModal');
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    var modal = document.getElementById('tagModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
