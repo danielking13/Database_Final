49ecd15c15321c3b9e022607d57b31a862f8ff18
jmw7cf <julia.wopata@gmail.com>
2017-12-06 11:58:56 -0600

    Changed index.html to home.html for "Home" in nav

--- TableView.php
+++ TableView.php
@@ -134,7 +134,7 @@ class TableView
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">Home</a>
+                    <a class="nav-link" href="home.html">Home</a>
                 </li>
                 <li class="nav-item active">
                     <a class="nav-link" href="table.php">Data Table</a><span class="sr-only">(current)</span>
